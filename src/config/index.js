938cc2cd4eb23647c6611245f9a6d805384777a3
Hassan Mohamed <hassan.mhmd656@gmail.com>
2023-10-26 22:04:22 +0300

    add firebase emulator flag

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,8 @@ dotenv.config();
 const config = {
   app: {
     port: process.env.PORT || 5000,
-    adminUsername: process.env.ADMIN_USERNAME || 'hsnAdmin',
-    adminPassword: process.env.ADMIN_PASSWORD || 'admin1234',
+    adminUsername: process.env.ADMIN_USERNAME || "hsnAdmin",
+    adminPassword: process.env.ADMIN_PASSWORD || "admin1234",
   },
   jwt: {
     secret: process.env.JWT_SECRET || "not-jwt-secret",
@@ -12,7 +12,10 @@ const config = {
   algolia: {
     appId: process.env.ALGOLIA_APP_ID || "",
     searchKey: process.env.ALGOLIA_SEARCH_KEY || "",
-  }
+  },
+  firebase: {
+    env: process.env.FIRE_BASE_ENV || "emulators",
+  },
 };
 
 module.exports = config;
--- src/firebase/firestoreDb.js
+++ src/firebase/firestoreDb.js
@@ -2,15 +2,18 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
 const serviceAccount = require("./firebase.secret.json");
+const config = require("../config");
 
 initializeApp({
   credential: cert(serviceAccount),
 });
 
-getFirestore().settings({
-  host: "localhost:8080",
-  ssl: false,
-});
+if (config.firebase.env === "emulators") {
+  getFirestore().settings({
+    host: "localhost:8080",
+    ssl: false,
+  });
+}
 
 const firestoreDb = getFirestore();
 
